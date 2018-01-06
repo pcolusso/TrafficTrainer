ea4f05b0cf4acc43b9b61b009aceb187202a3721
Paul Colusso <paulcolusso@gmail.com>
2018-01-06 20:38:55 +1100

    Initial Commit

---
+++ TrafficTrainer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TrafficTrainer
+//
+//  Created by Paul Colusso on 6/1/18.
+//  Copyright © 2018 Paul Colusso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
